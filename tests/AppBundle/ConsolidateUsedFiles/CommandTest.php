950ff2be5b40ab73d8d23012a13a18cb97428069
Malte Wunsch <malte.wunsch@googlemail.com>
2017-08-05 18:49:55 +0200

    Test the ConsolidateUsedFiles console command

---
+++ tests/AppBundle/ConsolidateUsedFiles/CommandTest.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace AppBundle\ConsolidateUsedFiles;
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * Tests for the ConsolidateUsedFiles console command class.
+ */
+final class CommandTest extends KernelTestCase
+{
+    const PATH_TO_FIXTURE = __DIR__ . '/fixtures/files-to-consolidate.txt';
+
+    /** @var Command */
+    private $command;
+
+    /** @var CommandTester */
+    private $commandTester;
+
+    protected function setUp()
+    {
+        // set up command tester
+        self::bootKernel();
+        $application = new Application(self::$kernel);
+        $application->add(new Command(new Task()));
+        $this->command = $application->find('consolidate-used-files');
+
+        $this->commandTester = new CommandTester($this->command);
+    }
+
+    /**
+     * @test
+     */
+    public function successOutput()
+    {
+        $this->commandTester->execute([
+            'command'  => $this->command->getName(),
+            'usedFiles' => self::PATH_TO_FIXTURE,
+        ]);
+
+        $output = $this->commandTester->getDisplay();
+        $this->assertContains('Finished', $output);
+    }
+
+    /**
+     * @test
+     */
+    public function fileGetsConsolidated()
+    {
+        // set up fixture
+        file_put_contents(self::PATH_TO_FIXTURE, implode(PHP_EOL, ['b', 'a', 'a']));
+
+        $this->commandTester->execute([
+            'command'  => $this->command->getName(),
+            'usedFiles' => self::PATH_TO_FIXTURE,
+        ]);
+
+        $result = file(self::PATH_TO_FIXTURE, FILE_IGNORE_NEW_LINES);
+        $this->assertEquals(['a', 'b'], $result);
+    }
+
+    /**
+     * @test
+     */
+    public function nonExistingFileGivesError()
+    {
+        $this->commandTester->execute([
+            'command'  => $this->command->getName(),
+            'usedFiles' => 'non-existing-file',
+        ]);
+
+        $output = $this->commandTester->getDisplay();
+        $this->assertContains('[ERROR]', $output);
+        $this->assertContains('file', $output);
+    }
+
+    /**
+     * @test
+     */
+    public function nonReadableFileGivesError()
+    {
+        $this->commandTester->execute([
+            'command'  => $this->command->getName(),
+            'usedFiles' => __DIR__ . '/fixtures/readable-but-not-writable-file.txt',
+        ]);
+
+        $output = $this->commandTester->getDisplay();
+        $this->assertContains('[ERROR]', $output);
+        $this->assertContains('readable', $output);
+    }
+
+    /**
+     * @test
+     */
+    public function nonWritableFileGivesError()
+    {
+        // make file not readable (it's not checked in that way since git couldn't read it that way).
+        $pathToWritableButNotReadableFixture = __DIR__ . '/fixtures/writable-but-not-readable-file.txt';
+        $originalRights = fileperms($pathToWritableButNotReadableFixture);
+        chmod($pathToWritableButNotReadableFixture, 0200);
+
+        $this->commandTester->execute([
+            'command'  => $this->command->getName(),
+            'usedFiles' => $pathToWritableButNotReadableFixture,
+        ]);
+
+        // restore original rights so git does not recognise a modification
+        chmod($pathToWritableButNotReadableFixture, $originalRights);
+
+        $output = $this->commandTester->getDisplay();
+        $this->assertContains('[ERROR]', $output);
+        $this->assertContains('writable', $output);
+    }
+}
